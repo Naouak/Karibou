0eec973fcd5468c94e56c7d18681a312b38aead1
Antoine Leclercq <antoine.leclercq@gmail.com>
2006-01-03 02:30:56 +0000

    NetCV : allcv listing started

---
+++ apps/netcv/allcv.class.php
@@ -0,0 +1,24 @@
+<?php 
+
+/**
+ * @copyright 2005 Antoine Leclercq <http://antoine.leclercq.netcv.org>
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.gnu.org/licenses/gpl.html.
+ * 
+ * @package applications
+ **/
+ 
+
+class NetCVAllCV extends Model
+{
+	public function build()
+	{
+		$CVDB = new NetCVDB($this->db);
+		$allcv = $CVDB->getAllCV();
+		
+		$this->assign("allcv", $allcv);
+	}
+}
+
+?>
---
+++ apps/netcv/class/netcvdb.class.php
@@ -0,0 +1,49 @@
+<?php
+
+class NetCVDB {
+
+	protected $db;
+	protected $cvlist;
+	
+	function __construct(PDO $db)
+	{
+		$this->db = $db;
+	}
+	
+	function getAllCV ()
+	{
+				echo $allcvReq = "
+				SELECT
+					netcv_resumes_group.id, netcv_resumes_group.user_id, netcv_resumes_group.hostname, netcv_resumes_group.diffusion,
+					netcv_resumes_by_lang.id, netcv_resumes_by_lang.group_id, netcv_resumes_by_lang.jobtitle as langjobtitle,
+					netcv_users.username, netcv_users.firstname, netcv_users.lastname, netcv_users.jobtitle,
+					TIMESTAMP(MAX(netcv_resumes.date_modification)) as last_modification
+				FROM
+					netcv_resumes_group, netcv_resumes_by_lang, netcv_users, netcv_resumes
+				WHERE 
+						netcv_resumes_group.id = netcv_resumes_by_lang.group_id
+					AND
+						netcv_users.id = netcv_resumes_group.user_id
+					AND
+						netcv_resumes.resume_id = netcv_resumes_by_lang.id
+				GROUP BY
+					netcv_resumes.resume_id
+				ORDER BY
+					username";
+				
+				/* LEFT JOIN netcv_skins ON netcv_skins.id = netcv_resumes_group.skin_id */
+				$allcvRes = $this->db->prepare($allcvReq);
+				$allcvRes->execute();
+
+				if ( $allcvRows = $allcvRes->fetchAll(PDO::FETCH_ASSOC))
+				{
+					$this->cvlist = $allcvRows;
+				}
+				else
+				{
+					$this->cvlist = FALSE;
+				}
+				return $this->cvlist;
+	}
+}
+?>
