b92496157163af558b2840617493c27f1db927e1
Antoine Leclercq <antoine.leclercq@gmail.com>
2006-01-12 09:17:51 +0000

    Framework : minor change on execution timer

--- karibou2/class/executiontimer.class.php
+++ karibou2/class/executiontimer.class.php
@@ -90,7 +90,7 @@ class ExecutionTimer
 		$html = "";
 		if( $this->display == false ) return $html;
 		
-		$html .= "<div style=\"text-align: left; border: 2px solid #000000; position: absolute; top:0px; right:0px; background: #ffffff;\"><pre>";
+		$html .= "<div style=\"text-align: left; border: 2px solid #000000; position: absolute; top:0px; right:0px; background: #ffffff; height: 400px; overflow: scroll; padding-right: 20px;\"><pre>";
 
 		reset($this->timeArray);
 		arsort($this->timeArray);
