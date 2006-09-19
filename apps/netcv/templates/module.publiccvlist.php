cba86df854626a5b85bb18bea516d9e91e1e3802
Antoine Leclercq <antoine.leclercq@gmail.com>
2006-09-19 04:07:23 +0000

    NetCV : correction du module d'affichage de la liste des CV

--- apps/netcv/templates/module.publiccvlist.php
+++ apps/netcv/templates/module.publiccvlist.php
@@ -2,39 +2,37 @@
 	$lang = $this->vars['myNetCVLanguage'];
 	if (isset($this->vars['groups']) && count($this->vars['groups'])>0)
 	{
-		echo "<h3>NetCV : ".count($this->vars['groups']).' Curriculum Vitae</h3>';
-		/*
-		if (count($this->vars['groups'])>1) 
-			echo _('NETCV_CVs');
-		else
-			echo _('NETCV_CV');
-		echo " :";
-		*/
-		echo '<ul>';
+		$r = '';
 		foreach($this->vars['groups'] as $group)
 		{
 			if ($group->getInfo('diffusion') != 'private')
 			{
-				echo '<li>';
-				echo '<a href="http://'.$group->getInfo('hostname').'.'.$GLOBALS['config']['netcv']['host'].'">http://'.$group->getInfo('hostname').'.'.$GLOBALS['config']['netcv']['host'].'</a>';
+				$r .= '<li>';
+				$r .= '<a href="http://'.$group->getInfo('hostname').'.'.$GLOBALS['config']['netcv']['host'].'">http://'.$group->getInfo('hostname').'.'.$GLOBALS['config']['netcv']['host'].'</a>';
 				$cvlist = $group->returnCVList();
 				if (count($cvlist)>0)
 				{
-					echo " (";
+					$r .= " (";
 					$n = 0;
 					foreach($cvlist as $cv)
 					{
 						if ($n > 0)
-							echo ', ';
-						echo '<a href="http://'.$group->getInfo('hostname').'.'.$GLOBALS['config']['netcv']['host'].'/'.$cv->getInfo('lang').'" title="http://'.$group->getInfo('hostname').'.'.$GLOBALS['config']['netcv']['host'].'/'.$cv->getInfo('lang').'">'.$lang->getNameByCode($cv->getInfo('lang')).'</a>';
+							$r .= ', ';
+						$r .= '<a href="http://'.$group->getInfo('hostname').'.'.$GLOBALS['config']['netcv']['host'].'/'.$cv->getInfo('lang').'" title="http://'.$group->getInfo('hostname').'.'.$GLOBALS['config']['netcv']['host'].'/'.$cv->getInfo('lang').'">'.$lang->getNameByCode($cv->getInfo('lang')).'</a>';
 						$n++;
 					}
-					echo ")";
+					$r .= ")";
 				}
-				echo '</li>';
+				$r .= '</li>';
 			}
-		}
+		}		
+	}
+	
+	if (isset($r) && count($r)>0)
+	{
+		echo "<h3>NetCV : ".count($this->vars['groups']).' Curriculum Vitae</h3>';
+		echo '<ul>';
+		echo $r;
 		echo '</ul>';
-		
 	}
 ?>
