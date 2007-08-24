9ff32eb5f4620620f9c04be0b28910fb5b4c1d68
Antoine Leclercq <antoine.leclercq@gmail.com>
2007-08-24 23:15:16 +0000

    Modifications mineures

--- apps/robots.txt/templates/default.php
+++ apps/robots.txt/templates/default.php
@@ -1,5 +1,8 @@
 <?
 		header("Content-Type: text/plain");
 		echo "User-agent: *\n";
-		echo "Disallow:\n";
+		//echo "Allow:/netcv/allcv/";
+		echo "Disallow:/minichat\n";
+		echo "Disallow:/news\n";
+		echo "Disallow:/wiki\n";
 ?>
