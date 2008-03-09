a4dd6f05a55505fd8e2180a15220ed42e30235b0
TELnet <telnet-tl1@googlegroups.com>
2008-03-09 22:46:17 +0000

    Remove the ugly menu on the left...

--- apps/header/templates/body.php
+++ apps/header/templates/body.php
@@ -6,26 +6,7 @@
 
 	<div id="container">
 		<a href="<?=kurl(array('app'=>'', 'page'=>''));?>#" id="logo"><span><h1>Karibou</h1></span></a>
-	
-			<?
-				if (isset($this->hookManager->hooks['header_menu']))
-				{
-			?>
-		<div id="main" class="nav">
-			<div id="navigation">
-				<ul>
-					<? hook(array('name'=>"header_menu")); ?>
-				</ul>
-			</div>
-			<?
-				}
-				else
-				{
-			?>
 		<div id="main" class="nonav">
-			<?
-				}
-			?>
 			<div id="content1">
 				<div id="content">
 <? hook(array('name'=>"page_content_start")); ?>
