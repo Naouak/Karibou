e9e000f1c7effeb667dc05086de568a26589dd39
TELnet <telnet-tl1@googlegroups.com>
2008-03-14 23:59:27 +0000

    Fix a part of this crap, but need a rewrite to clean this mess.

--- website/themes/js/hintbox.js
+++ website/themes/js/hintbox.js
@@ -1,40 +1,38 @@
-var offsetxpoint=-60 //Customize x offset of tooltip
-var offsetypoint=20 //Customize y offset of tooltip
+var offsetxpoint=-60; //Customize x offset of tooltip
+var offsetypoint=20; //Customize y offset of tooltip
 
-var dall=document.all
-var geid=document.getElementById && !document.all
+var dall=document.all;
+var geid=document.getElementById && !document.all;
 
-var enablehint=false
+var enablehint=false;
+var hintobject = null;
 
-if (dall||geid)
-{
-	var hintobject = document.all? document.all["hintbox"] : document.getElementById? document.getElementById("hintbox") : ""
-}
+Event.observe(window, "load", function() {
+	if (document.getElementById) {
+		hintobject = document.getElementById("hintbox");
+	} else if (document.all) {
+		hintobject = document.all["hintbox"];
+	}
+});
+
+Event.observe(document, "mousemove", positiontip);
 
 function truebody()
 {
-	return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
+	return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body;
 }
 
 function showhint(diplayText, displayClass)
 {
-	if (geid||dall)
+	if (hintobject)
 	{
-	/*
-		if (typeof thewidth!="undefined")
-		{
-			tipobj.style.width=thewidth+"px"
-		}
-	*/
-		
-		if (typeof displayClass!="undefined" && displayClass!="")
-		{
-			hintobject.className = 'hint ' + displayClass
+		if (typeof displayClass!="undefined" && displayClass!="") {
+			hintobject.className = 'hint ' + displayClass;
 		}
 		
-		hintobject.innerHTML = diplayText
-		enablehint = true
-		return false
+		hintobject.innerHTML = diplayText;
+		enablehint = true;
+		return false;
 	}
 }
 
@@ -46,52 +44,45 @@ function positiontip(e)
 		var curY=(geid)?e.pageY : event.clientY + truebody().scrollTop;
 		
 		//Find out how close the mouse is to the corner of the window
-		var rightedge = dall&&!window.opera? truebody().clientWidth-event.clientX-offsetxpoint : window.innerWidth-e.clientX-offsetxpoint-20
-		var bottomedge = dall&&!window.opera? truebody().clientHeight-event.clientY-offsetypoint : window.innerHeight-e.clientY-offsetypoint-20
-		var topedge = dall&&!window.opera? event.clientY+offsetypoint : e.clientY+offsetypoint-20
+		var rightedge = dall&&!window.opera? truebody().clientWidth-event.clientX-offsetxpoint : window.innerWidth-e.clientX-offsetxpoint-20;
+		var bottomedge = dall&&!window.opera? truebody().clientHeight-event.clientY-offsetypoint : window.innerHeight-e.clientY-offsetypoint-20;
+		var topedge = dall&&!window.opera? event.clientY+offsetypoint : e.clientY+offsetypoint-20;
 		
-		var leftedge = (offsetxpoint<0)? offsetxpoint*(-1) : -1000
+		var leftedge = (offsetxpoint<0)? offsetxpoint*(-1) : -1000;
 		
 		//if the horizontal distance isn't enough to accomodate the width of the context menu
 		if (rightedge < hintobject.offsetWidth)
 		{
 			//move the horizontal position of the menu to the left by it's width
-			hintobject.style.left=dall? truebody().scrollLeft + event.clientX - hintobject.offsetWidth+"px" : window.pageXOffset+e.clientX - hintobject.offsetWidth+"px"
+			hintobject.style.left=dall? truebody().scrollLeft + event.clientX - hintobject.offsetWidth+"px" : window.pageXOffset+e.clientX - hintobject.offsetWidth+"px";
 		}
 		else if (curX < leftedge)
 		{
-			hintobject.style.left="5px"
+			hintobject.style.left="5px";
 		}
 		else
 		{
 			//position the horizontal position of the menu where the mouse is positioned
-			hintobject.style.left = curX + offsetxpoint + "px"
+			hintobject.style.left = curX + offsetxpoint + "px";
 		}
 
 		//same concept with the vertical position
-		if (bottomedge < hintobject.offsetHeight)
-		{
-			hintobject.style.top = dall? truebody().scrollTop + event.clientY - hintobject.offsetHeight - offsetypoint+"px" : window.pageYOffset + e.clientY - hintobject.offsetHeight - offsetypoint+"px"
+		if (bottomedge < hintobject.offsetHeight) {
+			hintobject.style.top = dall? truebody().scrollTop + event.clientY - hintobject.offsetHeight - offsetypoint+"px" : window.pageYOffset + e.clientY - hintobject.offsetHeight - offsetypoint+"px";
+		} else {
+			hintobject.style.top = curY+offsetypoint + "px";
 		}
-		else
-		{
-			hintobject.style.top = curY+offsetypoint + "px"
-		}
-
-		hintobject.style.visibility = "visible"
-
-
+		hintobject.style.visibility = "visible";
 	}
 }
 
-function hidehint(){
-	if (geid||dall){
-		enablehint = false
-		hintobject.style.visibility="hidden"
-		hintobject.style.backgroundColor=''
-		hintobject.style.width=''
-		hintobject.style.myClass='hint'
+function hidehint() {
+	if (hintobject) {
+		enablehint = false;
+		hintobject.style.visibility = "hidden";
+		hintobject.style.backgroundColor = '';
+		hintobject.style.width = '';
+		hintobject.style.myClass = 'hint';
 	}
 }
 
-document.onmousemove=positiontip
