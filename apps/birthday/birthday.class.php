28db70763284f8111ac4887e375ea626b394d2d0
Pierre Ducroquet <pinaraf@pinaraf.info>
2008-03-29 17:11:43 +0000

    Continue simplifying this code... This reduces the number of queries. Kill dead code.

--- apps/birthday/birthday.class.php
+++ apps/birthday/birthday.class.php
@@ -12,31 +12,18 @@
 
 class Birthday extends Model
 {
-	public function build()
-	{
-	
-		$app = $this->appList->getApp($this->appname);
-		$config = $app->getConfig();
-	
-		$frameworkdb = $GLOBALS['config']['bdd']['frameworkdb'];
-
-        $query = "SELECT u.login,p.birthday from ".$frameworkdb.".profile p, ".$frameworkdb.".users u where MONTH(p.birthday) = MONTH(CURRENT_DATE()) AND DAY(p.birthday) = DAY(CURRENT_DATE()) and u.profile_id=p.id";	
-		$bdayers = Array();
-		foreach($this->db->query($query) as $row){
-			$userobj = $this->userFactory->prepareUserFromLogin($row['login']);
-			$this->userFactory->setUserList();
-			$user['user'] = $userobj;
-			$user['login'] = $row['login'];
-			$user['firstname'] = $userobj->getFirstname();
-			$user['lastname'] = $userobj->getLastname();
-			$user['nickname'] = $userobj->getSurname();
-			$user['age'] = explode("-",$row['birthday']);
-			$user['age'] =  date("Y") - $user['age'][0];
-			$bdayers[] = $user;
-		}	
-		$this->assign("islogged", $this->currentUser->isLogged());	
-		$this->assign('bdayers', $bdayers);
-	}
+    public function build()
+    {
+        $frameworkdb = $GLOBALS['config']['bdd']['frameworkdb'];
+        $query = "SELECT u.id, YEAR(p.birthday) AS birthYear from ".$frameworkdb.".profile p, ".$frameworkdb.".users u where MONTH(p.birthday) = MONTH(CURRENT_DATE()) AND DAY(p.birthday) = DAY(CURRENT_DATE()) and u.profile_id=p.id";	
+        $bdayers = Array();
+        foreach($this->db->query($query) as $row) {
+            $user['user'] = $this->userFactory->prepareUserFromId($row['id']);
+            $user['age'] =  date("Y") - $row["birthYear"];
+            $bdayers[] = $user;
+        }
+        $this->assign('bdayers', $bdayers);
+    }
 }
 
 ?>
