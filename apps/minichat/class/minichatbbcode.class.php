11067272adb82fd63854195b46de0c10f292f0d3
Vincent Billey <fenn59@gmail.com>
2008-05-29 00:58:54 +0000

    Addition of bold, italic and replacement for URLs

--- apps/minichat/class/minichatbbcode.class.php
+++ apps/minichat/class/minichatbbcode.class.php
@@ -18,9 +18,15 @@ class MinichatBBCode
 	
 	function transform($in)
 	{
-		$patterns = array("/\[url\](.*)\[\/url\]/U",
-				  "/\[color=(#[1-9a-fA-F]{3,6}|[a-z]*)\](.*)\[\/color\]/U");
-		$replaces = array("<a href=\"\\1\" target=\"_blank\">\\1</a>",
+		$patterns = array("/\[url=(.*)\](.*)\[\/url\]/Ui",
+				  "/\[url\](.*)\[\/url\]/Ui",
+				  "/\[b\](.*)\[\/b\]/Ui",
+				  "/\[i\](.*)\[\/i\]/Ui",
+				  "/\[color=(#[1-9a-fA-F]{3,6}|[a-z]*)\](.*)\[\/color\]/Ui");
+		$replaces = array("<a href=\"\\1\" target=\"_blank\">\\2</a>",
+				  "<a href=\"\\1\" target=\"_blank\">\\1</a>",
+				  "<span style=\"font-weight:bold;\">\\1</span>",
+				  "<span style=\"font-style:italic;\">\\1</span>",
 				  "<span style=\"color:\\1;\">\\2</span>");
 		$out = preg_replace($patterns, $replaces, $in);
 		return $out;
