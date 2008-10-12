f0f80392b24685f9d04860485630bc24f8b623cd
TELnet <telnet-tl1@googlegroups.com>
2008-10-12 22:15:10 +0000

    Forgot one file

---
+++ apps/stats/statsconfig.class.php
@@ -0,0 +1,18 @@
+<?php
+/* BEWARE made by Naouak*/ 
+ 
+/**
+ * Classe StatsConfig
+ *
+ * @package applications
+ */
+
+class StatsConfig extends Model
+{
+	public function build()
+	{
+	}
+	
+	
+}
+?>
