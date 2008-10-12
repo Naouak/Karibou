cd53aec4689489db898e9f8c83a2c9e259deb639
TELnet <telnet-tl1@googlegroups.com>
2008-10-12 22:14:58 +0000

    Cleanups...

--- apps/stats/stats.class.php
+++ apps/stats/stats.class.php
@@ -31,10 +31,10 @@ class Stats extends Model
 	{
 		// Create the views if needed...
 		try {
-			$this->db->exec("CREATE VIEW minichat_stats_temp AS (SELECT id_auteur AS auteur, post, DATE(`time`) AS msgDate FROM minichat WHERE post LIKE 'preums' OR post LIKE 'deuz' OR post LIKE 'troiz' OR post LIKE 'dernz' ORDER BY `time`)");
-			$this->db->exec("CREATE VIEW minichat_preums AS (SELECT auteur, msgDate FROM minichat_stats_temp WHERE post LIKE 'preums' GROUP BY msgDate)");
-			$this->db->exec("CREATE VIEW minichat_deuz AS (SELECT mt.auteur, mt.msgDate FROM minichat_stats_temp mt WHERE mt.post LIKE 'deuz' AND NOT(mt.auteur IN (SELECT mp.auteur FROM minichat_preums mp WHERE mp.msgDate = mt.msgDate)) GROUP BY mt.msgDate)");
-			$this->db->exec("CREATE VIEW minichat_troiz AS (SELECT mt.auteur, mt.msgDate FROM minichat_stats_temp mt WHERE mt.post LIKE 'troiz' AND NOT(mt.auteur IN (SELECT mp.auteur FROM minichat_preums mp WHERE mp.msgDate = mt.msgDate)) AND NOT(mt.auteur IN (SELECT md.auteur FROM minichat_deuz md WHERE md.msgDate=mt.msgDate)) GROUP BY mt.msgDate);");
+			$this->db->exec("CREATE VIEW minichat_stats_temp AS (SELECT id_auteur AS auteur, post, DATE(`time`) AS msgDate, `time` AS msgTime FROM minichat WHERE post LIKE 'preums' OR post LIKE 'deuz' OR post LIKE 'troiz' OR post LIKE 'dernz' ORDER BY `time`)");
+			$this->db->exec("CREATE VIEW minichat_preums AS (SELECT auteur, msgDate FROM minichat_stats_temp WHERE post LIKE 'preums' GROUP BY msgDate ORDER BY msgTime)");
+			$this->db->exec("CREATE VIEW minichat_deuz AS (SELECT mt.auteur, mt.msgDate FROM minichat_stats_temp mt WHERE mt.post LIKE 'deuz' AND NOT(mt.auteur IN (SELECT mp.auteur FROM minichat_preums mp WHERE mp.msgDate = mt.msgDate)) GROUP BY mt.msgDate ORDER BY msgTime)");
+			$this->db->exec("CREATE VIEW minichat_troiz AS (SELECT mt.auteur, mt.msgDate FROM minichat_stats_temp mt WHERE mt.post LIKE 'troiz' AND NOT(mt.auteur IN (SELECT mp.auteur FROM minichat_preums mp WHERE mp.msgDate = mt.msgDate)) AND NOT(mt.auteur IN (SELECT md.auteur FROM minichat_deuz md WHERE md.msgDate=mt.msgDate)) GROUP BY mt.msgDate ORDER BY msgTime);");
 		} catch (PDOException $e) {
 			// Just ignore, it means the view already exists...
 		}
@@ -85,7 +85,7 @@ class Stats extends Model
 		}
 		
 		//récupération des dernz
-		$stmt = $this->db->prepare("SELECT m1.id_auteur AS champion, COUNT(m1.id)*:score AS score FROM minichat m1 WHERE m1.post LIKE :dernz AND m1.id=(SELECT id FROM minichat WHERE DATE(`time`)=DATE(m1.time) ORDER BY `time` DESC LIMIT 1) GROUP BY id_auteur");
+/*		$stmt = $this->db->prepare("SELECT m1.id_auteur AS champion, COUNT(m1.id)*:score AS score FROM minichat m1 WHERE m1.post LIKE :dernz AND m1.id=(SELECT id FROM minichat WHERE DATE(`time`)=DATE(m1.time) ORDER BY `time` DESC LIMIT 1) GROUP BY id_auteur");
 
 		$stmt->bindParam(':dernz', $dernz["text"]);
 		$stmt->bindValue(':score', $dernz["score"],PDO::PARAM_INT);
@@ -95,7 +95,7 @@ class Stats extends Model
 				$results[$row[0]] += $row[1];
 			else
 				$results[$row[0]] = $row[1];
-		}
+		}*/
 		
 		//tri des scores
 		arsort($results);
--- apps/stats/templates/stats.tpl
+++ apps/stats/templates/stats.tpl
@@ -2,8 +2,8 @@
 
 <span class="StatsContent">
 	
-	<h4>##CHAMPIONSOFPREUMS##:</h4>
-	<ol style="list-style-type:decimal">
+	<h4><a onclick="new Effect.toggle(document.getElementById('statsPreumsList')); return false;" >##CHAMPIONSOFPREUMS##</a></h4>
+	<ol style="list-style-type:decimal; display: none;" id="statsPreumsList">
 		{foreach name=outer item=contact from=$contacts}
 		{if $smarty.foreach.outer.index<10}
 		<li>
@@ -13,8 +13,8 @@
 		{/foreach}
 	</ol>
 	
-	<h4>##CHAMPIONSOFFLOOD##:</h4>
-	<ol style="list-style-type:decimal">
+	<h4><a onclick="new Effect.toggle(document.getElementById('statsFloodList')); return false;" >##CHAMPIONSOFFLOOD##</a></h4>
+	<ol style="list-style-type:decimal; display:none;" id="statsFloodList">
 {foreach name=outer item=contact from=$flooderlist}
 {if $smarty.foreach.outer.index<10}
 		<li>
@@ -23,9 +23,7 @@
 {/if}
 {/foreach}
 	</ol>
-	
 	<span>
-		<a href="{kurl app="stats" page=""}">##PREUMSBIGLINK##</a>
+		<a href="{kurl page=""}">##PREUMSBIGLINK##</a>
 	</span>
-
 </span>
