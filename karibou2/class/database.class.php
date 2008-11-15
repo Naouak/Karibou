c94df7cc07b72484488431ff4cdeb37f925d319d
Quentin Burny <tardiel@gmail.com>
2008-11-15 02:22:14 +0000

    SET NAMES utf8 !

--- karibou2/class/database.class.php
+++ karibou2/class/database.class.php
@@ -24,6 +24,8 @@ Class Database extends PDO
 		parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 		if (parent::getAttribute(PDO::ATTR_DRIVER_NAME) == 'mysql')
 			parent::setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+		
+		$this->exec("SET NAMES utf8;");
 	}
 	
 	function query($qry)
