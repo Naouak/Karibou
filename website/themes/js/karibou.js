88be4fad89882c0b093d717654dadc06561c03e0
Pierre Ducroquet <pinaraf@pinaraf.info>
2008-11-29 23:43:38 +0000

    Big cleanup

--- website/themes/js/karibou.js
+++ website/themes/js/karibou.js
@@ -21,69 +21,30 @@ var KSortable = Object.extend(Sortable, {
           encodeURIComponent(items[i].id) );
 
     return queryComponents.join("&");
-  },
-  
-  dropAll: function(element) {
-    var element = $(element);
-    var sortableOptions = this.options(element);
-    var options = Object.extend({
-      tag:  sortableOptions.tag,
-      only: sortableOptions.only,
-      name: element.id
-    }, arguments[1] || {});
-    
-    var items = $(element).childNodes;
-    var queryComponents = new Array();
-    
-    for(var i=0; i<items.length; i++)
-    {
-      if(items[i].tagName && items[i].tagName==options.tag.toUpperCase() &&
-        ( !options.only || (Element.Class.has(items[i], options.only)) ) )
-      {
-           Effect.Fade( items[i].id );
-      }
-    }
   }
 });
 
-		/**
-		 * Ouverture d'un popup
-		 *
-		 * La méthode popup permet d'ouvrir une fenêtre aux dimensions et positions voulues
-        */
-		function popup(adresse, nom, hauteur, largeur, haut, gauche){
-            window.open(adresse, nom,'menubar=false, status=false, location=false, scrollbar=false, resizable=false, height='+hauteur+', width='+largeur+', top='+haut+', left='+gauche);
-        }
+/**
+ * Open a popup.
+ */
+function popup(url, name, height, width, top, left) {
+	window.open(url, name, "menubar=false, status=false, location=false, scrollbar=false, resizable=false, height='+height+', width='+width+', top='+top+', left='+left);
+}
 
 /**
-* Gestion des raccourcis clavier
+* Keyboard shortcuts handler
 */
 Event.observe(document, "keypress", function(evt) {
-    var key, inputType;
-    
-    //Récupération des touches pressées
-    if (window.event) {
-        // Pour Internet Explorer
-        key = window.event.keyCode;
-        inputType = window.event.srcElement.tagName.toLowerCase();
-    } else {
-        // Pour FireFox & autres
-        key = evt.which;
-        inputType = evt.target.nodeName.toLowerCase()
-    }
-
-    // Désactive les raccourcis lorsque le focus est dans les champs de saisie
-    if (inputType != 'input' && inputType != 'textarea' && inputType != 'select') {
-        // Efface la touche tapée sous ie pour éviter qu'elle soit transmise et affichée dans le champ 
-        // cible dans le cas d'un focus
-        if (window.event)
-            window.event.keyCode = "";
-
+	var key = evt.charCode;
+	var inputType = evt.element().tagName.toLowerCase();
+	// Disable keyboard shortcuts when an input field is focused
+	if (inputType != 'input' && inputType != 'textarea' && inputType != 'select') {
 		// r : refresh flashmails
-        if (key == 114)
-            FlashmailManager.Instance.refreshFlashmails();
-        // f : focus sur la boite rechercher
-        if (key == 102)
-            document.forms['search'].elements['keywords'].focus();
-    }
+		if (key == 114)
+			FlashmailManager.Instance.refreshFlashmails();
+		// f : focus search field
+		if (key == 102)
+			document.forms['search'].elements['keywords'].focus();
+	}
 });
+
