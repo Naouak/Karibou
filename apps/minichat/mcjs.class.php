ce3512232a1e512c27b402fad78033749f84effc
Pierre Ducroquet <pinaraf@pinaraf.info>
2009-02-03 12:43:28 +0000

    Fix the refresh of the minichat in karibou '2.5'

--- apps/minichat/mcjs.class.php
+++ apps/minichat/mcjs.class.php
@@ -2,6 +2,9 @@
 
 class MCJS extends Model {
 	public function build() {
+		$app = $this->appList->getApp($this->appname);
+		$config = $app->getConfig();
+		$this->assign("refreshInterval", $config["refresh"]["small"]);
 	}
 }
 ?>
