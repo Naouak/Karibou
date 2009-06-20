c948453516a39ee48e4c4f2bccfc0931f411f53f
Rémy Sanchez <remy.sanchez@hyperthese.net>
2009-06-20 04:11:27 +0000

    Per-app score management and score history. A view is automaticaly created with total scores. Of course there is changes in the API, they should talk by themselves
    New DB schema :
    
    DROP TABLE IF EXISTS `scores`;
    CREATE TABLE `scores` (
      `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
      `user_id` int(11) NOT NULL,
      `score` int(11) NOT NULL,
      `app` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
      `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
      PRIMARY KEY (`id`)
    ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;

--- daemons/score/ScoreFactory.class.php
+++ daemons/score/ScoreFactory.class.php
@@ -23,15 +23,21 @@ class ScoreFactory {
 	}
 
 	public static function initialize($db) {
+		try {
+			$db->query("CREATE VIEW scores_total AS SELECT user_id, SUM(score) AS score, app FROM scores GROUP BY user_id, app");
+		} catch (PDOException $e) {
+			// do nothing
+		}
+
 		self::$instance = new ScoreManager($db);
 	}
 
-	public static function addScoreToUser(User $user, $score) {
-		self::getInstance()->addScoreToUser($user, $score);
+	public static function addScoreToUser(User $user, $score, $app = "none") {
+		self::getInstance()->addScoreToUser($user, $score, $app);
 	}
 
-	public static function getScoreOf(User $user) {
-		return sefl::getInstance()->getScoreOf($user);
+	public static function getScoreOf(User $user, $app = null) {
+		return self::getInstance()->getScoreOf($user, $app);
 	}
 }
 ?>
--- daemons/score/ScoreManager.class.php
+++ daemons/score/ScoreManager.class.php
@@ -6,27 +6,40 @@ class ScoreManager {
 		$this->db = $db;
 	}
 
-	public function addScoreToUser(User $user, $score) {
+	public function addScoreToUser(User $user, $score, $app = "none") {
 		$query =
-			"INSERT INTO scores (user_id, score) ".
-			"VALUES (:user, :score) ".
-			"ON DUPLICATE KEY UPDATE ".
-			"score = score + :score ";
+			"INSERT INTO scores (user_id, score, app) ".
+			"VALUES (:user, :score, :app) ";
 		$sth = $this->db->prepare($query);
 		$sth->bindValue(":score", $score);
 		$sth->bindValue(":user", $user->getID());
+		$sth->bindValue(":app", $app);
 		$sth->execute();
 	}
 
-	public function getScoreOf(User $user) {
-		$query =
-			"SELECT SUM(score) AS score".
-			"FROM scores ".
-			"WHERE user_id = :user ";
-		$sth = $this->db->prepare($query);
-		$sth->bindValue(":user", $user->getID());
-		$sth->execute();
+	public function getScoreOf(User $user, $app = null) {
+		if(is_null($app)) {
+			$query =
+				"SELECT SUM(score) AS score ".
+				"FROM scores_total ".
+				"WHERE user_id = :user ";
+			$sth = $this->db->prepare($query);
+			$sth->bindValue(":user", $user->getID());
+			$sth->execute();
 
-		return $sth->fetchColumn(0);
+			return $sth->fetchColumn(0);
+		} else {
+			$query =
+				"SELECT SUM(score) AS score ".
+				"FROM scores_total ".
+				"WHERE user_id = :user ".
+				"AND app = :app";
+			$sth = $this->db->prepare($query);
+			$sth->bindValue(":user", $user->getID());
+			$sth->bindValue(":app", $app);
+			$sth->execute();
+
+			return $sth->fetchColumn(0);
+		}
 	}
 }
