03d592336fd8c4892cf15f5a76875c2c745f8e2b
Gilles Dehaudt <tonton1728@gmail.com>
2009-09-10 11:05:55 +0000

    this is an improvement of the daemon to send flashmail when there is dday

--- daemons/flashdayd/ddayflashlistener.class.php
+++ daemons/flashdayd/ddayflashlistener.class.php
@@ -2,6 +2,7 @@
 
 /**
  * @copyright 2009 Gilles DEHAUDT <tonton1728@gmail.com>
+ * @copyright 2009 Pierre DUCROQUET <pinaraf@pinaraf.info>
  *
  * @license http://www.gnu.org/licenses/gpl.html GNU Public License
  * See the enclosed file COPYING for license information (GPL).
@@ -18,48 +19,10 @@ class DDayFlashListener extends Listener
 {
 	function eventOccured(Event $event)
 	{
-		$sql = "SELECT event from dday where `date` = CURRENT_DATE";
-		try
-		{
-			$stmt = $this->db->prepare($sql);
-			$stmt->execute();
-		}
-		catch(PDOException $e)
-		{
-			Debug::kill($e->getMessage());
-		}
-		while ( ($fetch=$stmt->fetch(PDO::FETCH_ASSOC)) !== false)
-		{
-			$message = "Aujourd'hui, n'oublie pas : " . $fetch["event"];
-			$user_id = $this->currentUser->getID();
-			$sql1 = "SELECT id from flashmail where date(`date`)=CURRENT_DATE and message = :message and to_user_id = :user_id";
-			try {
-				$stmt1 = $this->db->prepare($sql1);
-				$stmt1->bindValue(":message", $message);
-				$stmt1->bindValue(":user_id", $user_id);
-				$stmt1->execute();
-			}
-			catch(PDOException $e)
-			{
-				Debug::kill($e->getMessage());
-			}
-			$fetch1=$stmt1->fetch(PDO::FETCH_ASSOC);
-			if ($fetch1["id"]==false)
-			{
-				$sql2="INSERT into flashmail (`date`,from_user_id,to_user_id,message) VALUES (NOW(), :fromid, :toid,:message)";
-				try
-				{
-					$stmt2 = $this->db->prepare($sql2);
-					$stmt2->bindValue(":fromid", "0");
-					$stmt2->bindValue(":toid", $user_id);
-					$stmt2->bindValue(":message", $message);
-					$stmt2->execute();
-				}
-				catch(PDOException $e)
-				{
-					Debug::kill($e->getMessage());
-				}
-			}
-		}
+		$this->db->exec("INSERT INTO flashmail(`date`, from_user_id, to_user_id, message) 
+    SELECT NOW(), 0, o.user_id, d.event 
+	FROM (dday d, onlineusers o) 
+	LEFT JOIN flashmail f ON (f.to_user_id = o.user_id AND f.message=d.event AND DATE(f.`date`) = CURRENT_DATE()) 
+	WHERE f.id IS NULL AND d.`date` = CURRENT_DATE();");
 	}
 }
