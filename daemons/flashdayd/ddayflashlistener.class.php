e42b6aeae45ec3dfccab3f632fd14e6b6ca3ee0a
Gilles Dehaudt <tonton1728@gmail.com>
2009-09-14 16:45:40 +0000

    small improvement in this daemon, the message is change to "aujourd'hui n'oublie pas : event" instead of "event"

--- daemons/flashdayd/ddayflashlistener.class.php
+++ daemons/flashdayd/ddayflashlistener.class.php
@@ -19,10 +19,11 @@ class DDayFlashListener extends Listener
 {
 	function eventOccured(Event $event)
 	{
+		$message = "'aujourd\'hui n\'oublie pas : '";
 		$this->db->exec("INSERT INTO flashmail(`date`, from_user_id, to_user_id, message) 
-    SELECT NOW(), 0, o.user_id, d.event 
+    SELECT NOW(), 0, o.user_id, CONCAT(".$message.",d.event) 
 	FROM (dday d, onlineusers o) 
-	LEFT JOIN flashmail f ON (f.to_user_id = o.user_id AND f.message=d.event AND DATE(f.`date`) = CURRENT_DATE()) 
+	LEFT JOIN flashmail f ON (f.to_user_id = o.user_id AND f.message=CONCAT(".$message.",d.event) AND DATE(f.`date`) = CURRENT_DATE()) 
 	WHERE f.id IS NULL AND d.`date` = CURRENT_DATE();");
 	}
 }
