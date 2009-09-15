85c4981b8a975b49a06fabe4fa45d65b2e16fa01
Gilles Dehaudt <tonton1728@gmail.com>
2009-09-15 13:30:36 +0000

    it's user_id 1 who sents flashmail and not 0.

--- daemons/flashdayd/ddayflashlistener.class.php
+++ daemons/flashdayd/ddayflashlistener.class.php
@@ -21,9 +21,9 @@ class DDayFlashListener extends Listener
 	{
 		$message = "'aujourd\'hui n\'oublie pas : '";
 		$this->db->exec("INSERT INTO flashmail(`date`, from_user_id, to_user_id, message) 
-    SELECT NOW(), 0, o.user_id, CONCAT(".$message.",d.event) 
+    SELECT NOW(), 1, o.user_id, CONCAT(".$message.",d.event) 
 	FROM (dday d, onlineusers o) 
 	LEFT JOIN flashmail f ON (f.to_user_id = o.user_id AND f.message=CONCAT(".$message.",d.event) AND DATE(f.`date`) = CURRENT_DATE()) 
-	WHERE f.id IS NULL AND d.`date` = CURRENT_DATE();");
+	WHERE f.id IS NULL AND d.`date` = CURRENT_DATE() AND f.from_user_id=1;");
 	}
 }
