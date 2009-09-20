fbda774f1b335f471916cc896d2f2c902b442e29
Gilles Dehaudt <tonton1728@gmail.com>
2009-09-20 13:05:18 +0000

    Same error mistake I made with flashdayd

--- daemons/flashpoll/pollflashlistener.class.php
+++ daemons/flashpoll/pollflashlistener.class.php
@@ -22,8 +22,8 @@ class PollFlashListener extends Listener
 		$message = "'n\'oublie pas de voter pour ce nouveau sondage : '"; 
 		$this->db->exec("INSERT INTO flashmail (`date`, from_user_id, to_user_id, message) 
 		SELECT NOW(), 1, o.user_id, CONCAT(".$message.",p.question) FROM (onlineusers AS o , polls AS p)
-		LEFT JOIN flashmail AS f ON (f.to_user_id=o.user_id AND f.message = CONCAT(".$message.",p.question)) 
+		LEFT JOIN flashmail AS f ON (f.to_user_id=o.user_id AND f.message = CONCAT(".$message.",p.question)) AND f.from_user_id=1
 		LEFT JOIN poll_votes AS v ON v.user=o.user_id AND v.poll=p.id 
-		WHERE f.message IS NULL AND p.id=(SELECT MAX(id) FROM polls) AND v.id IS NULL AND f.from_user_id=1;");
+		WHERE f.message IS NULL AND p.id=(SELECT MAX(id) FROM polls) AND v.id IS NULL ;");
 	}
 }
