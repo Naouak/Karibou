63571e25e933b8d149a96649f96f6e1a625f80f8
Gilles Dehaudt <tonton1728@gmail.com>
2009-10-08 08:02:43 +0000

    small fix

--- apps/createapps/create.py
+++ apps/createapps/create.py
@@ -1,18 +1,18 @@
-#! /user/bin/env python
+#! /usr/bin/env python
 import os
 import sys
 
 # definition of all fonctions 
 
-def creationofdir(self,dir,name):
+def creationofdir(dir,name):
     os.mkdir("../"+dir+"/"+name)
 
-def creationoffile(self,dir,name,extension):
+def creationoffile(dir,name,extension):
     fichier = open("../"+arg+dir+"/"+name+"."+extension,"w+")
     fichier.close()
 
-def parsingoffile(self,filesource,filedestination,variable):
-    fs = open("../"+filesource, "r")
+def parsingoffile(filesource,filedestination,variable):
+    fs = open(filesource, "r")
     fd = open("../"+filedestination,"w")
 
     data = fs.read()
@@ -35,12 +35,12 @@ def cleaningoffile(file):
     
     d="\n".join(lines)
     f.close()
-    f=open(file,"w")
+    f=open("../"+file,"w")
     f.write(d)
     f.close()
 
 #we check if we are in the right directory
-if (not(os.path.exists("../apps"))):
+if (os.path.basename(os.getcwd())!="createapps"):
     print "you are not in the good path"
 else:
     # check if an argument were given 
@@ -49,24 +49,24 @@ else:
     else:
         arg = sys.argv[1]
         #check if the name is already use for the other apps
-        if (os.path.exists(arg)):
+        if (os.path.exists("../"+arg)):
             print "this name is already use for an application, please choose an other one"
         else:
             # creation of all needed files and directories
-            creationofdir("",".",arg)
+            creationofdir("",arg)
 
-            creationoffile("","","config","xml")
-            creationoffile("","",arg,"app2")
-            creationoffile("","",arg,"class.php")
+            creationoffile("","config","xml")
+            creationoffile("",arg,"app2")
+            creationoffile("",arg,"class.php")
 
-            creationofdir("",arg,"languages")
+            creationofdir(arg,"languages")
 
-            creationoffile("","/languages","en","po")
-            creationoffile("","/languages","fr","po")
+            creationoffile("/languages","en","po")
+            creationoffile("/languages","fr","po")
 
-            creationofdir("",arg,"templates")
+            creationofdir(arg,"templates")
 
-            creationoffile("","/templates","mini","tpl")
+            creationoffile("/templates","mini","tpl")
 
             app2 = {"name_fr" : "french name of the app","name_en" : "english name of the app", "desc_fr" : "french description of the app", "desc_en" : "english description of the app" }
             app2_response = {}
@@ -78,18 +78,18 @@ else:
             # all files needed are created, now we are going to create optional files
             promptconfig = raw_input("do you need system of config ? ")
             if (promptconfig in ["", "yes","y", "o","oui"]):
-                creationoffile("","",arg+"config","class.php")
+                creationoffile("",arg+"config","class.php")
                 app2_response["configmodel"]="config"
 
             promptsubmit = raw_input("do you need a configuration system ? ")
             if (promptsubmit in ["","yes","y","o","oui"]):
-                creationoffile("","",arg+"submit","class.php")
+                creationoffile("",arg+"submit","class.php")
                 app2_response["submitmodel"]="submit"
 
             promptjs = raw_input("do you want to use Javascript ? ")
             if (promptjs in ["","yes","y","o","oui"]):
-                creationoffile("","/templates","js","tpl")
+                creationoffile("/templates","js","tpl")
                 app2_response["jsview"]="JS"
 
-            parsingoffile("","base.app2",arg+"/"+arg+".app2",app2_response)
+            parsingoffile("base.app2",arg+"/"+arg+".app2",app2_response)
             cleaningoffile(arg+"/"+arg+".app2")
