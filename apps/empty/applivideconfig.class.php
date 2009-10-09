db563eaa27c1f7b8a22e4a7b453720f02795d307
Gilles Dehaudt <tonton1728@gmail.com>
2009-10-09 10:19:09 +0000

    add small some files into the emptyapp

---
+++ apps/empty/applivideconfig.class.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * @copyright 2008 Gilles Dehaudt
+ *
+ * @license http://www.gnu.org/licenses/gpl.html GNU Public License
+ * See the enclosed file COPYING for license information.
+ * 
+ * @package applications
+ **/
+// this is the file which permits to have the configuration system
+class name of the name of config extends AppConfigModel
+{
+
+    public function formFields()
+    {
+
+        // in this return, you should type, the configuration variable and if they are needed and what type they are
+	    return array();
+    }
+}
+
+?>
---
+++ apps/empty/applividesubmit.class.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+ * @copyright 2008 Gilles Dehaudt
+ *
+ * @license http://www.gnu.org/licenses/gpl.html GNU Public License
+ * See the enclosed file COPYING for license information.
+ * 
+ * @package applications
+ **/
+
+/** 
+ * 
+ * @package applications
+ **/
+//this is the class which permits to create the submit system 
+class AnnonceAdd extends AppContentModel
+{
+    // this fonction permits to use the information the user type 
+	public function submit($parameters)
+	{
+	}
+	// this fonction permits to define what information who want, what type they are, if they are needed or not, ...
+	public function formFields()
+	{
+	}
+}
