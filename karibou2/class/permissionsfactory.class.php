e652f8a51dd58343e19253ac402a23e68a6c2195
Gilles Dehaudt <tonton1728@gmail.com>
2009-10-15 07:19:33 +0000

    fix of the fixing

--- karibou2/class/permissionsfactory.class.php
+++ karibou2/class/permissionsfactory.class.php
@@ -44,7 +44,7 @@ class PermissionsFactory
 		{
 			$annudb = $GLOBALS['config']['bdd']['frameworkdb'];
 
-			if ($user->isGlobalAdmin($db)) {
+			if ($user->isGlobalAdmin($this->db)) {
 				$perm->setDefaultPermission(_ADMIN_);
 			} else {
 				$qry_groups = $user->getGroupTreeQuery();
