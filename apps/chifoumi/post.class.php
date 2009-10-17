d502c7217affc6f9748d009e85edbca0a2a6e6a9
Quentin Burny <tardiel@gmail.com>
2009-10-17 11:50:05 +0000

    chifoumi server-side anti-flood

--- apps/chifoumi/post.class.php
+++ apps/chifoumi/post.class.php
@@ -6,46 +6,33 @@ class ChifoumiPost extends FormModel
 		$bet = filter_input(INPUT_POST,"bet");
 		$weapon = filter_input(INPUT_POST,"weapon");
 		if(isset($bet) && isset($weapon)){
-			//For debugging purpose
-			/*
-			$stmt = $this->db->prepare("
-				INSERT INTO chifoumi(user,bet,weapon,dateofchallenge,challenged)
-					SELECT 
-						user_id as user, 
-						:bet as bet, 
-						:weapon as weapon, 
-						NOW() as dateofchallenge, 
-						:user as challenged
-					FROM
-						onlineusers
-					WHERE
-						user_id <> :user
-					ORDER BY 
-						RAND()
-					LIMIT 1
-			");
-			*/
-			$stmt = $this->db->prepare("
-				INSERT INTO chifoumi(user,bet,weapon,dateofchallenge,challenged)
-					SELECT 
-						:user as user, 
-						:bet as bet, 
-						:weapon as weapon, 
-						NOW() as dateofchallenge, 
-						user_id as challenged
-					FROM
-						onlineusers
-					WHERE
-						user_id <> :user
-					ORDER BY 
-						RAND()
-					LIMIT 1
-			");
-			
+			$stmt = $this->db->prepare("SELECT TIME_TO_SEC(TIMEDIFF(NOW(),dateofchallenge)) as diff FROM chifoumi WHERE user = :user ORDER BY id DESC LIMIT 1");
 			$stmt->bindValue(":user",$this->currentUser->getID());
-			$stmt->bindValue(":bet",$bet);
-			$stmt->bindValue(":weapon",$weapon);
 			$stmt->execute();
+			$data = $stmt->fetch();
+			if(empty($data) || $data['diff'] > 300){
+				$stmt = $this->db->prepare("
+					INSERT INTO chifoumi(user,bet,weapon,dateofchallenge,challenged)
+						SELECT 
+							:user as user, 
+							:bet as bet, 
+							:weapon as weapon, 
+							NOW() as dateofchallenge, 
+							user_id as challenged
+						FROM
+							onlineusers
+						WHERE
+							user_id <> :user
+						ORDER BY 
+							RAND()
+						LIMIT 1
+				");
+				
+				$stmt->bindValue(":user",$this->currentUser->getID());
+				$stmt->bindValue(":bet",$bet);
+				$stmt->bindValue(":weapon",$weapon);
+				$stmt->execute();
+			}
 			
 		}
 	}
