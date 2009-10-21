31f48bbde21a80bd479fdeb0e9e1191fb91156c7
Rémy Sanchez <remy.sanchez@hyperthese.net>
2009-10-21 10:54:48 +0000

    Now 2 modes for rumour : random or show all

--- apps/rumour/rumourconfig.class.php
+++ apps/rumour/rumourconfig.class.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 /**
  * @copyright 2009 Gilles DEHAUDT
  *
  * @license http://www.gnu.org/licenses/gpl.html GNU Public License
  * See the enclosed file COPYING for license information.
- * 
+ *
  * @package applications
  **/
 // this is the file which permits to have the configuration system
@@ -13,7 +13,10 @@ class rumourconfig extends AppConfigModel
 	public function formFields()
 	{
 		// in this return, you should type, the configuration variable and if they are needed and what type they are
-		return array("number"=>array("type"=>"int","required"=>false, "label" => _("Choisir une rumeur parmis les X dernières"), "value"=> "5"));
+		return array(
+			"number" => array("type" => "int", "required" => false, "label" => _("Nombre de rumeurs"), "value" => "5"),
+			"random" => array("type" => "bool", "label" => _("N'afficher qu'une rumeur aléatoire"), "value" => false)
+		);
 	}
 }
 
