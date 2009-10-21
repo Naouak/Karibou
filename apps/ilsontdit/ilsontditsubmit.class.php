beb37336b3e471f80a0311aaa2f7b2c299d337cc
Pierre Ducroquet <pinaraf@pinaraf.info>
2009-10-21 12:07:57 +0000

    Use the new content modification system

--- apps/ilsontdit/ilsontditsubmit.class.php
+++ apps/ilsontdit/ilsontditsubmit.class.php
@@ -2,6 +2,46 @@
 
 class IlsontditSubmit extends AppContentModel {
 
+	public function isModifiable($key) {
+		if ($this->app->getPermission() == _ADMIN_) {
+			return true;
+		}
+		$query = $this->db->prepare("SELECT reporter FROM ilsontdit WHERE id=:id");
+		$query->bindValue(":id", $key);
+		if (!$query->execute()) {
+			Debug::kill("Error while checking rights");
+		} else {
+			$row = $query->fetch();
+			if ($row[0] == $this->currentUser->getID())
+				return true;
+		}
+		return false;
+	}
+
+	public function modify ($key, $parameters) {
+		$query = $this->db->prepare("UPDATE ilsontdit SET `who`=:who, `group`=:group, `message`=:msg WHERE id=:id LIMIT 1");
+		$query->bindValue(":group", $parameters["group"]);
+		$query->bindValue(":who", $parameters["author"]);
+		$query->bindValue(":msg", $parameters["quote"]);
+		$query->bindValue(":id", intval($key));
+		if (!$query->execute()) {
+			Debug::kill("Error while updating");
+		}
+	}
+
+	public function fillFields($key, &$fields) {
+		$query = $this->db->prepare("SELECT `who`, `group`, `message` FROM ilsontdit WHERE id=:id");
+		$query->bindValue(":id", intval($key));
+		if (!$query->execute()) {
+			Debug::kill("Error while filling fields");
+		} else {
+			$row = $query->fetch();
+			$fields["author"]["value"] = $row["who"];
+			$fields["quote"]["value"] = $row["message"];
+			$fields["group"]["value"] = $row["group"];
+		}
+	}
+
 	public function submit($parameters) {
 		// Insérer ici le code pour gérer la réception des données
 		$query = $this->db->prepare("INSERT INTO ilsontdit (`reporter`, `group`, `who`, `message`, `date_report`) VALUES (:reporter, :group, :who, :msg, NOW())");
--- apps/ilsontdit/templates/ilsontdit.tpl
+++ apps/ilsontdit/templates/ilsontdit.tpl
@@ -2,7 +2,7 @@
 <p>
 <b>{$quote.author}</b><br />
 <p style="padding-left: 0.5em;">
-	<em>&#8220;&nbsp;{$quote.text|wordwrap:34:" ":true|nl2br}&nbsp;&#8221;</em> {if $quote.reporter == $currentuser || $isadmin}<a href="{kurl app="ilsontdit" page="modify2" id=$quote.id }">modifier</a>{/if}
+	<em>&#8220;&nbsp;{$quote.text|wordwrap:34:" ":true|nl2br}&nbsp;&#8221;</em> {if $quote.reporter == $currentuser || $isadmin}<a onclick="$app(this).modify({$quote.id}); return false;">modifier</a>{/if}
 </p>
 {/foreach}
 </p>
