2ebce1033d97953e0be64a5863fbcc75dc1507a4
Grégoire Leroy <lupuscramus@gmail.com>
2009-10-30 15:38:50 +0000

    My first commit, add support for localization pref

--- apps/preferences/preferenceslarge.class.php
+++ apps/preferences/preferenceslarge.class.php
@@ -2,6 +2,7 @@
 /**
  * @version $Id:  preferenceslarge.class.php,v 0.1 2005/06/26 10:52:56 dat Exp $
  * @copyright 2005 Antoine Leclercq <http://antoine.leclercq.netcv.org>
+ * @copyright 2009 Grégoire Leroy <lupuscramus@gmail.com>
  *
  * @license http://www.gnu.org/licenses/gpl.html GNU Public License
  * See the enclosed file COPYING for license information.
@@ -17,7 +18,8 @@ class PreferencesLarge extends Model
 {
 	public function build()
 	{
-		$this->assign('langpref', $this->userFactory->getCurrentUser()->getPref('lang') );
+		$this->assign('lang', $this->userFactory->getCurrentUser()->getPref('lang') );
+		$this->assign('localization', $this->userFactory->getCurrentUser()->getPref('localize'));
 		$this->assign('permission', $this->permission);
 	}
 }
--- apps/preferences/preferencespost.class.php
+++ apps/preferences/preferencespost.class.php
@@ -2,6 +2,7 @@
 /**
  * @version $Id:  preferencespost.class.php,v 0.1 2005/06/29 10:57:56 dat Exp $
  * @copyright 2005 Antoine Leclercq <http://antoine.leclercq.netcv.org>
+ * @copyright 2009 Grégoire Leroy <lupuscramus@gmail.com>
  *
  * @license http://www.gnu.org/licenses/gpl.html GNU Public License
  * See the enclosed file COPYING for license information.
@@ -18,8 +19,13 @@ class PreferencesPost extends FormModel
 	public function build()
 	{
 		$currentUser = $this->userFactory->getCurrentUser();
-		$prefName = 'lang';
-		$currentUser->setPref($prefName, $_POST[$prefName]);
+		$prefName = array('lang','localize');
+		
+		$lang = filter_input(INPUT_POST,"lang",FILTER_SANITIZE_SPECIAL_CHARS);
+		$currentUser->setPref($prefName[0], $lang);
+
+		$loc = filter_input(INPUT_POST, "localization", FILTER_SANITIZE_SPECIAL_CHARS);
+		$currentUser->setPref($prefName[1], $loc);
 
 	}
 }
