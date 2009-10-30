0774f1ff9d66233b3b3b41352b0d88e47fc37336
Pierre Ducroquet <pinaraf@pinaraf.info>
2009-10-30 22:39:38 +0000

    Fix indentation

--- apps/dday/dday.class.php
+++ apps/dday/dday.class.php
@@ -14,33 +14,33 @@
  **/
 class Dday extends Model
 {
-    public function build()
-    {
-	    $sql = "SELECT *, 
-		    DATE_FORMAT(`date`, \"%Y%m%d\") AS vcalDateStart,
-		    DATE_FORMAT(DATE_ADD(`date`, INTERVAL 1 DAY), \"%Y%m%d\") AS vcalDateEnd,
-		    (TO_DAYS(`date`) - TO_DAYS(CURRENT_DATE())) AS JJ 
-		    FROM `dday` WHERE date >= CURRENT_DATE() AND visible = '1' ORDER BY date LIMIT 5";
-        try
-        {
-            $stmt = $this->db->query($sql);
-        }
-        catch(PDOException $e)
-        {
-            Debug::kill($e->getMessage());
-        }
-        
-        if($ddaylist = $stmt->fetchAll(PDO::FETCH_ASSOC))
-        {
-            $this->assign("ddaylist",$ddaylist);
-        }
-        else
-        {
-            $this->assign("DDempty","Err empty");
-        }
-        $this->assign("isadmin",$this->getPermission() == _ADMIN_);
-        $this->assign("currentuser", $this->currentUser->getId());
-    }
+	public function build()
+	{
+		$sql = "SELECT *, 
+			DATE_FORMAT(`date`, \"%Y%m%d\") AS vcalDateStart,
+			DATE_FORMAT(DATE_ADD(`date`, INTERVAL 1 DAY), \"%Y%m%d\") AS vcalDateEnd,
+			(TO_DAYS(`date`) - TO_DAYS(CURRENT_DATE())) AS JJ 
+			FROM `dday` WHERE date >= CURRENT_DATE() AND visible = '1' ORDER BY date LIMIT 5";
+		try
+		{
+			$stmt = $this->db->query($sql);
+		}
+		catch(PDOException $e)
+		{
+			Debug::kill($e->getMessage());
+		}
+
+		if($ddaylist = $stmt->fetchAll(PDO::FETCH_ASSOC))
+		{
+			$this->assign("ddaylist",$ddaylist);
+		}
+		else
+		{
+			$this->assign("DDempty","Err empty");
+		}
+		$this->assign("isadmin",$this->getPermission() == _ADMIN_);
+		$this->assign("currentuser", $this->currentUser->getId());
+	}
 }
 
 ?>
