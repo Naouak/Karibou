5ff6341c0d3c7f18856b12fb1b86a7b99b849356
Pierre Ducroquet <pinaraf@pinaraf.info>
2009-10-31 00:36:57 +0000

    Hello world, fire up your phpMyAdmin/mycrapadmin/..., and fix again the votes table...

--- apps/votes/sql.sql
+++ apps/votes/sql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `karibou_apps`.`votes` (
 `id` SERIAL NOT NULL ,
-`key_id` INT NOT NULL ,
+`key_id` BIGINT UNSIGNED NOT NULL ,
 `user` INT NOT NULL ,
 `vote` INT NOT NULL
 ) ENGINE = InnoDB;
@@ -9,4 +9,8 @@ ALTER TABLE `votes` ADD UNIQUE (
 `key_id` ,
 `user`
 );
+
+ALTER TABLE `votes` ADD FOREIGN KEY ( `key_id` ) REFERENCES `karibou_apps`.`combox` (
+	`id`
+) ON DELETE CASCADE ON UPDATE CASCADE ;
 
