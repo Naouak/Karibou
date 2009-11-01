18e65af6d15f13fe1a05541982ef0f409e5c62c7
Rémy Sanchez <remy.sanchez@hyperthese.net>
2009-11-01 15:48:09 +0000

    gg=G

--- karibou2/class/ksmarty.class.php
+++ karibou2/class/ksmarty.class.php
@@ -31,28 +31,28 @@ class KSmarty extends Smarty
 	protected $appList;
 
 	protected $language;
-	
+
 	protected $hookManager;
-	
+
 	protected $currentLanguage;
-	
+
 	function KSmarty(AppList $appList, $hookManager, $currentLanguage="")
 	{
 		$this->Smarty();
 		$this->appList = $appList;
 		$this->currentLanguage = $currentLanguage;
-		
+
 		$this->register_function('kurl', 		'smarty_function_kurl');
 		$this->register_function('translate', 	'smarty_function_translate');
 		$this->register_function('userlink', 	'smarty_function_userlink');
 		$this->register_function('khint', 		'smarty_function_khint');
 		$this->register_block	('t', 			'smarty_block_t');
-        $this->register_modifier('find', 		'smarty_modifier_find');
-        $this->register_modifier('highlight',	'smarty_modifier_highlight');
-        $this->register_function('commentbox',	'smarty_function_commentbox');
-        $this->register_function('votesbox',	'smarty_function_votesbox');
-		
-		
+		$this->register_modifier('find', 		'smarty_modifier_find');
+		$this->register_modifier('highlight',	'smarty_modifier_highlight');
+		$this->register_function('commentbox',	'smarty_function_commentbox');
+		$this->register_function('votesbox',	'smarty_function_votesbox');
+
+
 		if ($hookManager !== FALSE)
 		{
 			$this->hookManager = $hookManager;
@@ -60,7 +60,7 @@ class KSmarty extends Smarty
 		$displayHook = array(&$this, 'displayHook');
 		$this->register_function('hook', $displayHook);
 
-		
+
 		//Utilisation d'un prefilter et non d'un postfilter pour permettre la compatibilité avec 
 		//la version précédente du gestionnaire de traductions (possibilité d'intégration de variables smarty)
 		$prefilterTranslation = array (&$this,'prefilterTranslation');
@@ -80,17 +80,17 @@ class KSmarty extends Smarty
 	public function getLanguage() {
 		return $this->currentLanguage;
 	}
-	
-	function fetch($_smarty_tpl_file, $_smarty_cache_id = null, $_smarty_compile_id = null, $_smarty_display = false) {
-	  // We need to set the cache id and the compile id so a new script will be
-	  // compiled for each language. This makes things really fast ;-)
-	  $_smarty_compile_id = $this->kapp . '-' . $this->currentLanguage . '-' . $_smarty_compile_id;
-	  $_smarty_cache_id = $_smarty_compile_id;
 
-	  // Now call parent method
-	  return parent::fetch( $_smarty_tpl_file, $_smarty_cache_id, $_smarty_compile_id, $_smarty_display );
+	function fetch($_smarty_tpl_file, $_smarty_cache_id = null, $_smarty_compile_id = null, $_smarty_display = false) {
+		// We need to set the cache id and the compile id so a new script will be
+		// compiled for each language. This makes things really fast ;-)
+		$_smarty_compile_id = $this->kapp . '-' . $this->currentLanguage . '-' . $_smarty_compile_id;
+		$_smarty_cache_id = $_smarty_compile_id;
+
+		// Now call parent method
+		return parent::fetch( $_smarty_tpl_file, $_smarty_cache_id, $_smarty_compile_id, $_smarty_display );
 	}
-	
+
 	/**
 	 * @returns : name of the application (string)
 	 */
@@ -109,50 +109,50 @@ class KSmarty extends Smarty
 	{
 		return $this->appList;
 	}
-	
-	
+
+
 	/**
-   * test to see if valid cache exists for this template
-   *
-   * @param string $tpl_file name of template file
-   * @param string $cache_id
-   * @param string $compile_id
-   * @return string|false results of {@link _read_cache_file()}
-   */
-   function is_cached($tpl_file, $cache_id = null, $compile_id = null)
-   {
-			if (!$this->caching)
-					 return false;
+	 * test to see if valid cache exists for this template
+	 *
+	 * @param string $tpl_file name of template file
+	 * @param string $cache_id
+	 * @param string $compile_id
+	 * @return string|false results of {@link _read_cache_file()}
+	 */
+	function is_cached($tpl_file, $cache_id = null, $compile_id = null)
+	{
+		if (!$this->caching)
+			return false;
 
-			if (!isset($compile_id)) {
-					 $compile_id = $this->currentLanguage.'-'.$this->compile_id;
-					 $cache_id = $compile_id;
-			}
+		if (!isset($compile_id)) {
+			$compile_id = $this->currentLanguage.'-'.$this->compile_id;
+			$cache_id = $compile_id;
+		}
 
-			return parent::is_cached($tpl_file, $cache_id, $compile_id);
-   }
-   
-   
-   
-  /**
-   * prefilterTranslation()
-   * Methode gerant la traduction du document
-   *
-   * @param $tpl_source
-   * @return
-   **/
-  function prefilterTranslation ($tpl_source, &$smarty)
-  {
-	$_translateFunction = array (&$this, '_translate_key');
-	
-	return preg_replace_callback('/##(.+?)##/', $_translateFunction, $tpl_source);
-  }
+		return parent::is_cached($tpl_file, $cache_id, $compile_id);
+	}
 
-  /**
-   * Methode traduisant le mot cle donne en parametre
-   * @param $key
-   * @return
-   */
+
+
+	/**
+	 * prefilterTranslation()
+	 * Methode gerant la traduction du document
+	 *
+	 * @param $tpl_source
+	 * @return
+	 **/
+	function prefilterTranslation ($tpl_source, &$smarty)
+	{
+		$_translateFunction = array (&$this, '_translate_key');
+
+		return preg_replace_callback('/##(.+?)##/', $_translateFunction, $tpl_source);
+	}
+
+	/**
+	 * Methode traduisant le mot cle donne en parametre
+	 * @param $key
+	 * @return
+	 */
 	function _translate_key($key)
 	{
 		return $this->translate_key($key[1]);
@@ -170,7 +170,7 @@ class KSmarty extends Smarty
 			$this->hookManager->display($param["name"]);
 		}
 	}
-	
+
 	function quick_hack_clear_values()
 	{
 		unset($this->_tpl_vars['config']);
