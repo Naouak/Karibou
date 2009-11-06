ee50b05ae06e6bfebe5b7de5ba2fae1b9c444c5f
Pierre Ducroquet <pinaraf@pinaraf.info>
2009-11-06 12:57:40 +0000

    Merge templates...

--- apps/header/templates/body.php
+++
@@ -1,8 +0,0 @@
-
-<body>
-	<div id="container">
-		<h1><a href="<?php echo kurl(array('app'=>'', 'page'=>''));?>#" id="logo"><span>Karibou</span></a></h1>
-		<div id="main" class="nonav">
-			<div id="content1">
-				<div id="content">
-<?php hook(array('name'=>"page_content_start")); ?>
--- apps/header/templates/headerhome.php
+++ apps/header/templates/headerhome.php
@@ -1,2 +1,40 @@
-<?php include('head.php'); ?>
-<?php include('body.php'); ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>';?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<base href="http://<?php echo $_SERVER['HTTP_HOST'].str_replace("index.php","",$_SERVER['SCRIPT_NAME']);?>" />
+
+	<meta name="robots" content="noindex, follow" />
+	<meta name="googlebot" content="noindex, follow" />
+	
+	<title>
+		<?php echo _('HEADER_PAGE_TITLE')." :: "._('KPOWERED');?>
+	</title>
+	<link rel="shortcut icon" type="image/x-icon" href="./themes/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="<?php echo $this->vars['karibou']['base_url'].$this->vars['cssFile'];?>" media="screen" title="Normal" />
+<?php
+	foreach($this->vars['styles'] as $style)
+	{
+?>
+		<link rel="alternate stylesheet" type="text/css" href="<?php echo $this->vars['karibou']['base_url'].$style['home_css'];?>" media="screen" title="<?php echo $style['titre'];?>" />
+<?php
+	}
+?>
+<!--[if lt IE 8]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE8.js" type="text/javascript"></script>
+<![endif]-->
+	<script type="text/javascript" src="<?php echo $this->vars['karibou']['base_url']?>/themes/js/prototype.js"></script>
+	<script type="text/javascript" src="<?php echo $this->vars['karibou']['base_url']?>/themes/js/scriptaculous.js"></script>
+	<script type="text/javascript" src="<?php echo $this->vars['karibou']['base_url']?>/themes/js/karibou.js"></script>
+	<script type="text/javascript" src="<?php echo $this->vars['karibou']['base_url']?>/themes/js/kform.js"></script>
+	<script type="text/javascript" src="<?php echo $this->vars['karibou']['base_url']?>/themes/js/hintbox.js"></script>
+<?php hook(array('name'=>"html_head")); ?>
+</head>
+<body>
+	<div id="container">
+		<h1><a href="<?php echo kurl(array('app'=>'', 'page'=>''));?>#" id="logo"><span>Karibou</span></a></h1>
+		<div id="main" class="nonav">
+			<div id="content1">
+				<div id="content">
+<?php hook(array('name'=>"page_content_start")); ?>
