a6e94a4c28a64404cc8dc47e202ccc2cff93ace1
Gilles Dehaudt <tonton1728@gmail.com>
2009-11-07 14:09:02 +0000

    Oops . Bug fix, I change fee weeks ago the class numberargument to add the support of relative numbers in page argument, so I had the support of - in this class, so the history of the minichat doesn't work anymore.

--- apps/arguments/relativenumberargument.class.php
+++ apps/arguments/relativenumberargument.class.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @copyright 2005 Jonathan Semczyk <jonathan.semczyk@free.fr>
+ * @copyright 2009 Gilles Dehaudt <tonton1728@gmail.com>
  *
  * @license http://www.gnu.org/licenses/gpl.html GNU Public License
  * See the enclosed file COPYING for license information.
@@ -9,15 +10,15 @@
  **/
  
 /**
- * Numeric Argument
+ * Relative Numeric Argument
  * 
  * @package applications
  */
-class NumberArgument extends Argument
+class RelativeNumberArgument extends Argument
 {
 	function getVar($arg)
 	{
-		if( preg_match('#([0-9]+)#', $arg, $match) )
+		if( preg_match('#([-]?[0-9]+)#', $arg, $match) )
 		{
 			return $match[1];
 		}
