9799053dee80760f87455bc475303e12f82b5ff7
Pierre Ducroquet <pinaraf@pinaraf.info>
2009-11-29 14:06:41 +0000

    Use the new feature of userlink, indent model code, improve (from a theorical point of view) the SQL query...

--- apps/birthday/birthday.class.php
+++ apps/birthday/birthday.class.php
@@ -1,7 +1,7 @@
 <?php 
 
 /**
- * @version $Id $
+ * @copyright 2009 Pierre Ducroquet <pinaraf@gmail.com>
  * @copyright 2007 Antoine Reversat
  *
  * See the enclosed file COPYING for license information (GPL). If you
@@ -12,19 +12,21 @@
 
 class Birthday extends Model
 {
-    public function build()
-    {
-        $frameworkdb = $GLOBALS['config']['bdd']['frameworkdb'];
-        $query = "SELECT u.id, YEAR(p.birthday) AS birthYear from ".$frameworkdb.".profile p, ".$frameworkdb.".users u where MONTH(p.birthday) = MONTH(CURRENT_DATE()) AND DAY(p.birthday) = DAY(CURRENT_DATE()) and u.profile_id=p.id";	
-        $bdayers = Array();
-        foreach($this->db->query($query) as $row) {
-            $user['user'] = $this->userFactory->prepareUserFromId($row['id']);
-            $user['age'] =  date("Y") - $row["birthYear"];
-            $bdayers[] = $user;
-        }
-        $this->assign('bdayers', $bdayers);
-        $this->assign("islogged", $this->currentUser->isLogged());
-    }
+	public function build()
+	{
+		$frameworkdb = $GLOBALS['config']['bdd']['frameworkdb'];
+		$query = "SELECT u.id, YEAR(CURRENT_DATE())-YEAR(p.birthday) AS age
+			FROM $frameworkdb.profile p
+			INNER JOIN $frameworkdb.users u ON u.profile_id=p.id
+			WHERE MONTH(p.birthday)=MONTH(CURRENT_DATE()) AND DAY(p.birthday)=DAY(CURRENT_DATE())";
+		$bdayers = Array();
+		foreach($this->db->query($query) as $row) {
+			$user['user'] = $this->userFactory->prepareUserFromId($row['id']);
+			$user['age'] = $row["age"];
+			$bdayers[] = $user;
+		}
+		$this->assign('bdayers', $bdayers);
+	}
 }
 
 ?>
--- apps/birthday/templates/small.tpl
+++ apps/birthday/templates/small.tpl
@@ -1,13 +1,13 @@
 {if count($bdayers) > 0}
 <h5>{t}Today we're wishing them a happy birthday:{/t}</h5>
 {foreach from=$bdayers item=bdayer }
-<span>{userlink user=$bdayer.user showpicture=$islogged} / {userlink user=$bdayer.user showpicture=$islogged showfullname=true}, {t count=$bdayer.age 1=$bdayer.age plural="%1 years old"}%1 year old{/t}</span>
+<span>{userlink user=$bdayer.user showpicture=$islogged showdisplayname=true showfullname=true}, {t count=$bdayer.age 1=$bdayer.age plural="%1 years old"}%1 year old{/t}</span>
 {if ($islogged)}
 {if (!$noflashmail)}
-		<a href="#" class="sendflashmaillink" onclick="FlashmailManager.Instance.newFlashmail('{$bdayer.user->getSurname()|escape:'javascript'}', {$bdayer.user->getId()}); return false;"><span>Flash</span></a>
+	<a href="#" class="sendflashmaillink" onclick="FlashmailManager.Instance.newFlashmail('{$bdayer.user->getSurname()|escape:'javascript'}', {$bdayer.user->getId()}); return false;"><span>Flash</span></a>
 {/if}
 {/if}
-		<br />
+	<br />
 {/foreach}
 {else}
 <h5>{t}No birthdays today{/t}</h5>
