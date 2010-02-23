53fd2b968f5dd31376977d54c1b5a08ab0d5d558
Pierre Ducroquet <pinaraf@pinaraf.info>
2010-02-23 01:57:29 +0100

    Add comments for the video application

--- apps/video/video.class.php
+++ apps/video/video.class.php
@@ -14,51 +14,61 @@
  **/
 class Video extends Model
 {
-    public function build()
-    {
-        $app = $this->appList->getApp($this->appname);
-        $config = $app->getConfig();
+	public function build()
+	{
+		$app = $this->appList->getApp($this->appname);
+		$config = $app->getConfig();
 
-        try
-        {
-            $stmt = $this->db->query("SELECT * FROM video WHERE datetime < NOW() AND deleted=0 ORDER BY datetime DESC LIMIT 5");
-        }
-        catch(PDOException $e)
-        {
-            Debug::kill($e->getMessage());
-        }
-
-	if ($videoonline = $stmt->fetchall(PDO::FETCH_ASSOC)) {
-		if ($user["object"] =  $this->userFactory->prepareUserFromId($videoonline[0]["user_id"])) {
-			$name=$this->appname."-".$videoonline[0]['id'];
-			$combox = new CommentSource($this->db,$name,"",$videoonline[0]["video"]);
-
-			$this->assign("idnow",$videoonline[0]["id"]);
-			$this->assign("idcomboxnow",$combox->getId());
-			$this->assign("videonow",$videoonline[0]["video"]);
-			$this->assign("commentaire",$videoonline[0]["comment"]);
-			$this->assign("url",$videoonline[0]["site"]);
-
-			foreach ($videoonline as $id => $items) {
-				$videoonline[$id]["user"] = $this->userFactory->prepareUserFromId($videoonline[$id]["user_id"]);
-				$name=$this->appname."-".$videoonline[$id]['id'];
-				$combox = new CommentSource($this->db,$name,"",$videoonline[$id]["video"]);
-				$videoonline[$id]["idcombox"] = $combox->getId();
-
-			}
-			
-			$this->assign("videosarray", $videoonline);
-			$this->assign("videoauthor",$user);
-			$this->assign("islogged", $this->currentUser->isLogged());
-			$this->assign("isadmin", $this->getPermission() == _ADMIN_);
-			
-		}
-		else
+		try
 		{
-			$this->assign("DDempty","Err empty");
+			$stmt = $this->db->query("SELECT * FROM video WHERE datetime < NOW() AND deleted=0 ORDER BY datetime DESC LIMIT 5");
+		}
+		catch(PDOException $e)
+		{
+			Debug::kill($e->getMessage());
+		}
+
+		if ($videoonline = $stmt->fetchall(PDO::FETCH_ASSOC)) {
+			if ($user["object"] =  $this->userFactory->prepareUserFromId($videoonline[0]["user_id"])) {
+				$name = $this->appname . '-' . $videoonline[0]['id'];
+
+				$videoUrl = $videoonline[0]['site'] . $videonline[0]['video'];
+				$videoHTML = "<object id=\"videoObject\" data=\"$videoUrl\" type=\"application/x-shockwave-flash\" width=\"200\" height=\"184\">";
+				$videoHTML .= "<param name=\"movie\" value=\"$videoUrl\" />";
+				$videoHTML .= "</object>";
+
+				$combox = new CommentSource($this->db, $name, $videoonline[0]["comment"], $videoHTML);
+
+
+				$this->assign("idnow",$videoonline[0]["id"]);
+				$this->assign("idcomboxnow",$combox->getId());
+				$this->assign("videonow",$videoonline[0]["video"]);
+				$this->assign("commentaire",$videoonline[0]["comment"]);
+				$this->assign("url",$videoonline[0]["site"]);
+
+				foreach ($videoonline as $id => $items) {
+					$videoonline[$id]["user"] = $this->userFactory->prepareUserFromId($videoonline[$id]["user_id"]);
+					
+					$videoUrl = $items['site'] . $items['video'];
+					$videoHTML = "<object id=\"videoObject\" data=\"$videoUrl\" type=\"application/x-shockwave-flash\" width=\"200\" height=\"184\">";
+					$videoHTML .= "<param name=\"movie\" value=\"$videoUrl\" />";
+					$videoHTML .= "</object>";
+
+					$name = $this->appname . '-' . $videoonline[$id]['id'];
+					$combox = new CommentSource($this->db, $name, $videoonline[$id]["comment"], $videoHTML);
+					$videoonline[$id]["idcombox"] = $combox->getId();
+				}
+				$this->assign("videosarray", $videoonline);
+				$this->assign("videoauthor",$user);
+				$this->assign("islogged", $this->currentUser->isLogged());
+				$this->assign("isadmin", $this->getPermission() == _ADMIN_);
+			}
+			else
+			{
+				$this->assign("DDempty","Err empty");
+			}
 		}
 	}
-    }
 }
 
 ?>
